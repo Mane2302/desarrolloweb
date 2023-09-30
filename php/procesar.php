471b300fdc8d70b0fca13129fd5755eba1ee97e6
Mane2302 <azoyu_roque@hotmail.com>
2023-09-30 17:05:48 -0600

    1

---
+++ php/procesar.php
@@ -0,0 +1,22 @@
+<?php
+
+$nombre = $_GET['nombre'];
+print("Su nombre es: ".$nombre);
+
+echo "<br>";
+$edad = $_GET['edad'];
+print("Su edad es: ".$edad);
+
+echo "<br>";
+$correo = $_GET['correo'];
+print("Su coreo es: ".$correo);
+
+echo "<br>";
+$telefono = $_GET['telefono'];
+print("Su telefono es: ".$telefono);
+
+echo "<br>";
+$sitioweb = $_GET['sitioweb'];
+print("Su sitio WEB es: ".$sitioweb);
+
+?>
